d40f1e0a416ec5988957f78e1f678e782f4edca1
Ayush Pandey <ayush.pandey@daffodilsw.com>
2021-08-19 21:45:39 +0530

    gitignore, clone and pull file added

---
+++ componentsClone.sh
@@ -0,0 +1,23 @@
+mkdir -p src/npms
+
+cd src/npms
+
+git clone https://github.com/ayushpandey05/core-components.git
+cd core-components
+# git config credential.helper store
+git checkout dev
+cd ..
+
+git clone https://github.com/ayushpandey05/colors.git
+cd colors
+# git config credential.helper store
+git checkout dev
+cd ..
+
+git clone https://github.com/ayushpandey05/icons.git
+cd icons
+# git config credential.helper store
+git checkout dev
+cd ..
+
+cd ../../
---
+++ componentsPull.sh
@@ -0,0 +1,18 @@
+git pull origin alpha
+
+cd  src/npms
+
+echo "*********************core-components***********************"
+cd core-components
+git pull origin dev
+
+echo "*********************colors***********************"
+cd ../
+cd colors
+git pull origin  dev
+
+echo "*********************icons***********************"
+cd ../
+cd icons
+git pull origin  dev
+
